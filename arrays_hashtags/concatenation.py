6837494ca944fe2f022ba6232a1b20256d99c1b2
isaac <olivareshernandezisaac@gmail.com>
2023-09-07 14:22:18 -0500

    completed #1929

---
+++ arrays_hashtags/concatenation.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def getConcatenation(self, num):
+        print(num*2)
+        return num*2
+
+
+solution = Solution()
+nums = [1,2,2]
+result = solution.getConcatenation(nums)
+print(result)
