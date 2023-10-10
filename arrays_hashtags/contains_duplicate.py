2f1f8c456dee724226559bb072ef902e5eb046b7
isaac <olivareshernandezisaac@gmail.com>
2023-10-10 12:16:23 -0500

    used 2 pointers to solve  it

--- arrays_hashtags/contains_duplicate.py
+++ arrays_hashtags/contains_duplicate.py
@@ -1,33 +1,34 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        newlist = sorted(nums)
-        print(newlist)
-        for i in range(len(newlist)-1):
-            # print(num)
-            print(i+1)
-            if newlist[i] == newlist[i+1]:
-                return True
-        return False
-
-
-solution = Solution()
-numss = [2,14,18,22,22,2]
-result = solution.containsDuplicate(numss)
-print(result)
-
-
-
-# #more efficient
-# class Solutionn:
-#     def containsDuplicate(self, numss):
-#         hashset = set()
-#         for n in numss:
-#             if n in hashset:
+# class Solution(object):
+#     def containsDuplicate(self, nums):
+#         newlist = sorted(nums)
+#         print(newlist)
+#         for i in range(len(newlist)-1):
+#             # print(num)
+#             print(i+1)
+#             if newlist[i] == newlist[i+1]:
 #                 return True
-#             hashset.add(n)
 #         return False
 
-# solution = Solutionn()
-# numss = [2,14,18,22,22]
+
+# solution = Solution()
+# numss = [2,14,18,22,22,2]
 # result = solution.containsDuplicate(numss)
 # print(result)
+
+
+
+# #more efficient for the runtime, hash set lookup runs in constant time O(n).
+class Solutionn:
+    def containsDuplicate(self, numss):
+        hashset = set()
+        print(hashset)
+        for n in numss:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+
+solution = Solutionn()
+numss = [2,14,18,22,22]
+result = solution.containsDuplicate(numss)
+print(result)
---
+++ arrays_hashtags/is_subsequence.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        i, j = 0,0
+        print(len(s),len(t))
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:        #in the first loop it compares "a" from s to "b" from t, since it is not the same then ..
+                i+=1                # i stays at 0 index,element "a" and  index j moves to 1(line 8 of the code). ...
+            j+=1                        #..Now it will compare s[0] to t[1]..since "a" == "a" the i will increase by one...
+                                        #now the if will compare s[1] to t[2], "b" =! "a" so it will only add 1 to j..
+        if i == len(s):                 # if will compare s[1] to t[3], "b" == "b", now it will exit the loop...
+            return True            
+        return False
+
+
+
+solution = Solution()
+s = "ab"
+t = "baab"
+nums = [1,2,2]
+result = solution.isSubsequence(s,t)
+print(result)
+
+
+# note: we can use j+=1 /n else: /n   j+=1 insted of just using j+=1 starting above line 8.
