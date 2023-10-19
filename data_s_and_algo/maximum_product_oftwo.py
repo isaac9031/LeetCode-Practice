26fd098a0b8cf7abdaaca85c400cfc3c0dacc18d
isaac <olivareshernandezisaac@gmail.com>
2023-10-19 17:44:21 -0500

    used slicing. used around the same amount of  memory, but a faster time

--- data_s_and_algo/maximum_product_oftwo.py
+++ data_s_and_algo/maximum_product_oftwo.py
@@ -1,20 +1,32 @@
 class Solution:
     def maxProduct(self, nums: list[int]) -> int:
-        maximum = max(nums[0],nums[1])
-        second_max = min(nums[0],nums[1])
-        for i in range(2, len(nums)):
-            if nums[i]>= maximum:
-                second_max = maximum
-                maximum = nums[i]
-            elif nums[i]> second_max:
-                second_max = nums[i]
-        return (maximum-1) * (second_max-1)
+        nums = sorted(nums)
+        return (nums[-1]-1)*(nums[-2]-1)
 
 
 nums = [3,4,5,2]
 solution = Solution()
 result = solution.maxProduct(nums)
 print(result)
+
+#another way to do it, consumes more memory and time
+# class Solution:
+#     def maxProduct(self, nums: list[int]) -> int:
+#         maximum = max(nums[0],nums[1])
+#         second_max = min(nums[0],nums[1])
+#         for i in range(2, len(nums)):
+#             if nums[i]>= maximum:
+#                 second_max = maximum
+#                 maximum = nums[i]
+#             elif nums[i]> second_max:
+#                 second_max = nums[i]
+#         return (maximum-1) * (second_max-1)
+
+
+# nums = [3,4,5,2]
+# solution = Solution()
+# result = solution.maxProduct(nums)
+# print(result)
 
 # Example 1:
 # Input: nums = [3,4,5,2]
