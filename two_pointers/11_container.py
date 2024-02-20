418cde7a31738dda4849be53e1e056c3b1d2bfb0
isaac <olivareshernandezisaac@gmail.com>
2024-02-20 09:53:04 -0600

    using brute force approach to understand the problem, then we use two-pointers and reuse some of the code from before

--- two_pointers/11_container.py
+++ two_pointers/11_container.py
@@ -4,37 +4,46 @@
 # Find two lines that together with the x-axis form a container, such that the container contains the most water.
 # Return the maximum amount of water a container can store.
 
-#Brute Force
-# class Solution:
-#     def maxArea(self, height: list[int]) -> int:
-#         res = 0
-#         for l in range(len(height)):
-#             for rp in range(l+1,len(height)):
-#                  area = (rp-l)*min(height[rp],height[l])
-#                  res = max(area, res)
-#         return res
-
-# container1 = Solution()
-# height = [1,8,6,2,5,4,8,3,7]
-# print(container1.maxArea(height))
-
-
+#Brute Force -> Then we use two pointer solution
 class Solution:
     def maxArea(self, height: list[int]) -> int:
+        # res = 0
+        # for i in range(len(height)):
+        #     for j in range(i+1, len(height)):
+        #         area = (j-i)*min(height[i],height[j])
+        #         res = max(res, area)
+        # return res
         res = 0
-        lp, rp = 0, len(height)-1
-        while lp < rp:
-            area = (rp-lp)*min(height[lp], height[rp])
+        lp = 0
+        rp = len(height)-1
+        while lp<rp:
+            area = (rp-lp)*min(height[lp],height[rp])
             res = max(area, res)
-
             if height[lp]<height[rp]:
                 lp+=1
             elif height[lp]>height[rp]:
                 rp-=1
-            else:  #if both heights are the same then we moved either pointer to get the rest of the areas that are inside.
+            else:
                 rp-=1
         return res
 
 container1 = Solution()
 height = [1,8,6,2,5,4,8,3,7]
 print(container1.maxArea(height))
+
+# Notes
+# Unlike the nested loop approach in your first solution, which..
+# ..iterates over all possible pairs, the two-pointer approach only requires a single pass ..
+# ..through the array. This significantly reduces the number of iterations and improves the algorithm's efficiency.
+
+
+# Brute-force approach: O(n^2)
+# Two-pointer approach: O(n)
+
+# In Big O notation:
+
+# O(n^2) means the algorithm's time complexity grows quadratically with the size of the..
+# ..input (i.e., for every increase in the input size by 1, the time it takes to run the algorithm increases by the square of 1).
+
+# O(n) means the algorithm's time complexity grows linearly with
+# the size of the input (i.e., for every increase in the input size by 1, the time it takes to run the algorithm increases by only 1).
