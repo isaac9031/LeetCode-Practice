eec02c9432e256e2a2a4f2c3d6319b1d1556f958
isaac <olivareshernandezisaac@gmail.com>
2024-05-10 10:30:35 -0500

    using sets data structures to find dubplicates

--- friday_challenges_redos/217_duplicate.py
+++ friday_challenges_redos/217_duplicate.py
@@ -1,12 +1,11 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        numbers = set(nums)
+        numbers = set()
         for num in nums:
-            if num not in numbers:
-                numbers.add(num)
-            else:
-                return False
-        return True
+            if num in numbers:
+                return True
+            numbers.add(num)
+        return False
 
 nums = [1,2,3,4]
 solution = Solution()
